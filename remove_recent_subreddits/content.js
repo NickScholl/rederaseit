e38a16492df78cac4e074e174da8515ae9e60b9e
nickk <68135932+NickScholl@users.noreply.github.com>
2024-10-02 22:02:57 -0300

    Delete remove_recent_subreddits directory

--- remove_recent_subreddits/content.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-    const shitty = document.getElementsByTagName("reddit-recent-pages")[0];
-    if (shitty) {
-        shitty.remove();
-    }
-    localStorage.removeItem("recent-subreddits-store");
-    localStorage.removeItem("recent-searches-store");
-}, 1000);
